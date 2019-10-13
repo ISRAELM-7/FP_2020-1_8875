6831479a98cdbabca400c3e3c76134e278b1f6b6
Israel Meraz Dionicio <merazdionicio@gmail.com>
2019-10-13 15:14:57 -0500

    Se agrega la practica

---
+++ TAREAS/3/main.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char*argv[]){
+	
+	double x;
+	double y;
+
+	x=atof (argv[1]);
+	y=atof (argv[2]);
+	
+	printf("%.2lf\n", x+y);
+	printf("%.2lf\n", x-y);
+	printf("%.2lf\n", x*y);
+	printf("%.2lf\n", x/y);
+	
+	return 0;
+	
+}
