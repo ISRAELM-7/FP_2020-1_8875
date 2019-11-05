0d4b45396718c8927d799254456593fb35e037fe
Israel Meraz Dionicio <merazdionicio@gmail.com>
2019-11-04 18:07:14 -0600

    se sube la tarea 7

--- TAREAS/7/main.c
+++ TAREAS/7/main.c
@@ -1,14 +1,19 @@
 #include <stdio.h> 
-#include <math.h> //utilice la libreria de matematicas paraq que lo pudiera resolver 
 #include <stdlib.h>
 
 int main(int argc, char*argv[])
 {
-	double x=atof(argv[1]); //se convierten el argv, y además noo es necesario un ciclo aya que entra dos variables
+	double resultado=1;
+//declare esta variable para utilizarlo en la multiplicacion
 	double y= atof(argv[2]);
-
-	double resultado =pow(x,y); //el pow es para las potencias 
+//la transsforme el argv[2] para su facil manejo y ademas solo estran dos parametros 
+	for(int i=0; i<y; i++){ //la inicialice en 0 y terminara hasta que sea meyor i  
+		double x=atof(argv[1]);
+ //se convierten el argv,y luego se efectua la multipliacion sin cambiar el parametro 1 
+        resultado=resultado*x;		
+	}
 	printf("%.2lf", resultado);
 
 	return 0;
 }
+
