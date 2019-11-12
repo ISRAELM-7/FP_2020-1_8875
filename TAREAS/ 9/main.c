ce8c5bb29032ca4ffcdba13dbac70563b1791e34
ISRAELM-7 <54034259+ISRAELM-7@users.noreply.github.com>
2019-11-12 15:54:11 -0600

    Tarea 9

---
+++ TAREAS/ 9/main.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+//Primero declaramos una función para que realice la operación suma
+int Suma(int numero){
+	int Suma=0;
+	int Modulo;
+	for(int i=1; i>0; i++){
+        Modulo=numero%10;
+        Suma=Suma+Modulo;
+//Para calcular los dígitos de un número primero es calcular el moduloy luego guardarlo Suma
+        numero=numero/10;
+//Luego cambiar quitar el dígito dividiendo entre 10 y tiene que regresar entero
+	}
+return Suma;
+}
+
+int main(int argc, char*argv[]){
+	int x=atoi(argv[1]);
+//Se transforma a un entero y luego se muestra el valor
+	printf("%d\n", Suma(x));
+return 0;
+}
