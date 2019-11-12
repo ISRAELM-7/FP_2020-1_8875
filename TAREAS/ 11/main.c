c30aeb261122041640964147c91e8bf2505b0533
ISRAELM-7 <54034259+ISRAELM-7@users.noreply.github.com>
2019-11-12 15:57:57 -0600

    Tarea 9 y 11

--- TAREAS/ 11/main.c
+++ TAREAS/ 11/main.c
@@ -1,16 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
+//Utilice la librería math para el valor absoluto
 
 double Raiz(double n, double tolerancia, double candidato){
 	double promedio;
-	double div;	
+	double div;
+//La condición es la diferencia de la división y el candidato para que sea mayor la tolerancia	
 while(fabs(div-candidato)>tolerancia){
 	div=n/candidato;
 	promedio=(div+candidato)/2;
 	candidato=promedio;
 }
 long double resultado= candidato;
+//El resultado se guarda en en largo double
 return resultado;
 }
 int main(int arg, char*argv[]){
@@ -18,5 +21,6 @@ int main(int arg, char*argv[]){
 	double y= atof(argv[2]);
 	double z= atof(argv[3]);
 	printf("%lf\n",Raiz(x,y,z));
+//No se utiliza .2 en el printf para que muestre todos los decimales
 	return 0;
 }
